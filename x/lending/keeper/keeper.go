4c7f0980cf31567240fdcd9c8f85f8b084974f80
Fausto Spoto <fausto.spoto@gmail.com>
2020-08-31 17:33:01 +0200

    Added keeper

--- x/lending/keeper/keeper.go
+++ x/lending/keeper/keeper.go
@@ -2,56 +2,161 @@ package keeper
 
 import (
 	"fmt"
-
-	"github.com/tendermint/tendermint/crypto"
-	"github.com/tendermint/tendermint/libs/log"
-
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/spoto/lending/x/lending/types"
 )
 
-// Keeper of the lending store
+// Keeper of the lending platform's store
 type Keeper struct {
+
+	// we include the keeper of Cosmos' Bank module
+	bankKeeper bank.Keeper
+
+	// we add an extra keeper to keep all debts created so far
 	storeKey   sdk.StoreKey
+
 	cdc        *codec.Codec
-	paramspace types.ParamSubspace
 }
 
-// NewKeeper creates a lending keeper
-func NewKeeper(cdc *codec.Codec, key sdk.StoreKey, paramspace types.ParamSubspace) Keeper {
-	keeper := Keeper{
-		storeKey:   key,
+func NewKeeper(storeKey sdk.StoreKey, bankKeeper bank.Keeper, cdc *codec.Codec) Keeper {
+	return Keeper{
+		storeKey:   storeKey,
+		bankKeeper: bankKeeper,
 		cdc:        cdc,
-		paramspace: paramspace.WithKeyTable(types.ParamKeyTable()),
 	}
-	return keeper
 }
 
-// Logger returns a module-specific logger.
-func (k Keeper) Logger(ctx sdk.Context) log.Logger {
-	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
+const debtStorePrefix = ":debt:"
+
+func getDebtStoreKey(ID string) []byte {
+	return []byte(debtStorePrefix + ID)
 }
 
-// Get returns the pubkey from the adddress-pubkey relation
-func (k Keeper) Get(ctx sdk.Context, key string) (/* TODO: Fill out this type */, error) {
-	store := ctx.KVStore(k.storeKey)
-	var item /* TODO: Fill out this type */
-	byteKey := []byte(key)
-	err := k.cdc.UnmarshalBinaryLengthPrefixed(store.Get(byteKey), &item)
+func (keeper Keeper) getDebtByID(ctx sdk.Context, id string) (types.Debt, error) {
+	store := ctx.KVStore(keeper.storeKey)
+
+	debtKey := getDebtStoreKey(id)
+	if !store.Has(debtKey) {
+		return types.Debt{}, fmt.Errorf("cannot find debt with ID %s", id)
+	}
+
+	var debt types.Debt
+	keeper.cdc.MustUnmarshalBinaryBare(store.Get(debtKey), &debt)
+	return debt, nil
+}
+
+func (keeper Keeper) CreateDebt(ctx sdk.Context, debt types.Debt) error {
+	store := ctx.KVStore(keeper.storeKey)
+
+	if !store.Has(getDebtStoreKey(debt.ID)) {
+		store.Set(getDebtStoreKey(debt.ID), keeper.cdc.MustMarshalBinaryBare(&debt))
+		return nil
+	}
+
+	return fmt.Errorf("cannot create a debt with an already used ID %s", debt.ID)
+}
+
+func (keeper Keeper) debtsIterator(ctx sdk.Context) sdk.Iterator {
+	store := ctx.KVStore(keeper.storeKey)
+	return sdk.KVStorePrefixIterator(store, []byte(debtStorePrefix))
+}
+
+// a functional type used to filter debts
+type debtDiscrimination func(debt types.Debt) bool
+
+func (keeper Keeper) getDebts(ctx sdk.Context, logic debtDiscrimination) []types.Debt {
+	ri := keeper.debtsIterator(ctx)
+	defer ri.Close()
+
+	receivedResult := []types.Debt{}
+	for ; ri.Valid(); ri.Next() {
+		var debt types.Debt
+		keeper.cdc.MustUnmarshalBinaryBare(ri.Value(), &debt)
+
+		// we only accept debts that satisfy the filter
+		if logic(debt) {
+			receivedResult = append(receivedResult, debt)
+		}
+	}
+
+	return receivedResult
+}
+
+func (keeper Keeper) GetAllDebts(ctx sdk.Context) []types.Debt {
+	// we use a filter that accepts everything
+	return keeper.getDebts(ctx, func(_ types.Debt) bool {
+		return true
+	})
+}
+
+func (keeper Keeper) GetDebtorDebts(ctx sdk.Context, address sdk.AccAddress) []types.Debt {
+	// we use a filter that projects on the debtor
+	return keeper.getDebts(ctx, func(debt types.Debt) bool {
+		return debt.Debtor.Equals(address)
+	})
+}
+
+func (keeper Keeper) GetCreditorDebts(ctx sdk.Context, address sdk.AccAddress) []types.Debt {
+	// we use a filter that projects on the creditor
+	return keeper.getDebts(ctx, func(debt types.Debt) bool {
+		return debt.Creditor.Equals(address)
+	})
+}
+
+func (keeper Keeper) PayDebt(ctx sdk.Context, msg types.MsgPayDebt) error {
+	debt, err := keeper.getDebtByID(ctx, msg.ID)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return item, nil
+
+	if !msg.Debtor.Equals(debt.Debtor) {
+		return fmt.Errorf("the debt with ID %s is not yours", msg.ID)
+	}
+
+	if err := keeper.bankKeeper.SendCoins(ctx, debt.Debtor, debt.Creditor, sdk.NewCoins(msg.Amount)); err != nil {
+		return err
+	}
+
+	if msg.Amount.IsLT(debt.Amount) {
+		debt.Amount = debt.Amount.Sub(msg.Amount)
+	} else {
+		debt.Amount = sdk.NewCoin(debt.Amount.Denom, sdk.NewInt(0))
+	}
+
+	return keeper.updateDebt(ctx, debt)
 }
 
-func (k Keeper) set(ctx sdk.Context, key string, value /* TODO: fill out this type */ ) {
-	store := ctx.KVStore(k.storeKey)
-	bz := k.cdc.MustMarshalBinaryLengthPrefixed(value)
-	store.Set([]byte(key), bz)
+func (keeper Keeper) ChangeDebt(ctx sdk.Context, msg types.MsgChangeDebt) error {
+	debt, err := keeper.getDebtByID(ctx, msg.ID)
+	if err != nil {
+		return err
+	}
+
+	if !msg.Creditor.Equals(debt.Creditor) {
+		return fmt.Errorf("the debt with ID %s is not yours", msg.ID)
+	}
+
+	if msg.Amount.IsGTE(debt.Amount) {
+		return fmt.Errorf("the new amount can only be smaller than the original %s", debt.Amount)
+	}
+
+	debt.Amount = debt.Amount.Sub(msg.Amount)
+	if debt.Amount.IsNegative() {
+		debt.Amount = sdk.NewCoin(debt.Amount.Denom, sdk.NewInt(0))
+	}
+
+	return keeper.updateDebt(ctx, debt)
 }
 
-func (k Keeper) delete(ctx sdk.Context, key string) {
-	store := ctx.KVStore(k.storeKey)
-	store.Delete([]byte(key))
+func (keeper Keeper) updateDebt(ctx sdk.Context, debt types.Debt) error {
+	store := ctx.KVStore(keeper.storeKey)
+
+	if !store.Has(getDebtStoreKey(debt.ID)) {
+		return fmt.Errorf("the debt with ID %s does not exist", debt.ID)
+	}
+
+	store.Set(getDebtStoreKey(debt.ID), keeper.cdc.MustMarshalBinaryBare(&debt))
+	return nil
 }

9cc1b2be0e8b1944f4d18e07b8976c5d28c8ba13
Fausto Spoto <fausto.spoto@gmail.com>
2020-09-01 16:50:35 +0200

    Fixed init.sh

--- init.sh
+++ init.sh
@@ -8,15 +8,15 @@ lendingD init mynode --chain-id lending
 
 lendingCLI config keyring-backend test
 
-lendingCLI keys add me
-lendingCLI keys add you
+lendingCLI keys add rich
+lendingCLI keys add poor
 
-lendingD add-genesis-account $(lendingCLI keys show me -a) 1000foo,100000000stake
-lendingD add-genesis-account $(lendingCLI keys show you -a) 1foo
+lendingD add-genesis-account $(lendingCLI keys show rich -a) 1000foo,100000000stake
+lendingD add-genesis-account $(lendingCLI keys show poor -a) 100foo
 
 lendingCLI config chain-id lending
 lendingCLI config output json
 lendingCLI config indent true
 lendingCLI config trust-node true
-lendingD gentx --name me --keyring-backend test
+lendingD gentx --name rich --keyring-backend test
 lendingD collect-gentxs
